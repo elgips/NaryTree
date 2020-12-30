01905f1204f12a7678db7648926da90ca5b31703
Elad <72931343+elgips@users.noreply.github.com>
2020-12-30 23:52:45 +0200

    Add files via upload

---
+++ point3D.h
@@ -0,0 +1,40 @@
+/*
+ * point3D.h
+ *
+ *  Created on: Dec 30, 2020
+ *      Author: gips
+ */
+
+#ifndef SRC_POINT3D_H_
+#define SRC_POINT3D_H_
+#include <vector>
+#include <string.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+class point3D{
+public:
+	double x;
+	double y;
+	double z;
+	point3D();
+	point3D(double _x,double _y,double _z);
+	point3D(const point3D& _p);
+	point3D Rx(double theta);
+	point3D Ry(double theta);
+	point3D Rz(double theta);
+	point3D MatrixRotate(double _M[3][3]);
+	double norm();
+	void normalize();
+	void set(double _x,double _y,double _z);
+	double DotProd(point3D _p);
+	/*project the point on a given normal vector*/
+	point3D projection(point3D _p_hat);
+	void gramSh(vector<point3D> _vp);
+	string point2String();
+};
+
+
+
+
+#endif /* SRC_POINT3D_H_ */
---
+++ streamLine3D.h
@@ -0,0 +1,35 @@
+/*
+ * streamLine3D.h
+ *
+ *  Created on: Dec 30, 2020
+ *      Author: gips
+ */
+
+#ifndef SRC_STREAMLINE3D_H_
+#define SRC_STREAMLINE3D_H_
+#include"point3D.h"
+//using namespace std;
+class streamLine3D{
+public:
+//	point3D *Xg,*Yg,*Zg;
+//	point3D *pI,*pH,*pL,*pU,*pE;
+	point3D *pI,*pE,*Xg,*Yg,*Zg,pH,pL,pU;
+	streamLine3D();
+	streamLine3D(point3D _pi);
+	streamLine3D(point3D* _pi);
+	streamLine3D(point3D _pi,point3D _pH0,point3D _pL0,point3D _pU0);
+	streamLine3D(point3D _pi,point3D _pH0);
+	streamLine3D(point3D _pi,point3D _pH0,point3D _pL0);
+	streamLine3D(point3D *_pi,point3D *_pe);
+	streamLine3D(streamLine3D *_s);
+	void updateDirRx(double _theta);
+	void updateDirRy(double _theta);
+	void updateDirRz(double _theta);
+	streamLine3D childStream(double dist);
+	void rotateStream(double _M[3][3]);
+	void rotateStreamAndPoints(double _M[3][3]);
+};
+
+
+
+#endif /* SRC_STREAMLINE3D_H_ */
